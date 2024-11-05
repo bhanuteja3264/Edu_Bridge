103bdd65ae5a9a8587cd83aae8a8b1a5082c3cba
48-JASHWANTH <ratnajashwanth64@gmail.com>
2024-11-05 12:53:49 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return(
+    <div>
+      <h1>Hello world</h1>
+      <Button>Click meee</Button>
+    </div>
+  )
+}
+
+export default App
