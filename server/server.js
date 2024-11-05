3e9f79e47728fdf7bcc2c3fb565d199af22fd65d
48-JASHWANTH <ratnajashwanth64@gmail.com>
2024-11-05 16:01:44 +0530

    DB connection

--- server/server.js
+++ server/server.js
@@ -5,6 +5,20 @@ const corsOptions = {
     origin :["http://localhost:5173"]
 }
 app.use(cors(corsOptions))
+require('dotenv').config();
+
+
+const mongodb = require('mongodb').MongoClient;
+
+let stinnovationDB;
+mongodb.connect(process.env.DB_URL)
+  .then(client => {
+    stinnovationDB = client.db('moviedb');
+    students = stinnovationDB.collection('students');
+    app.set('students', students);
+    console.log("DB connection successful!!!");
+  })
+  .catch(err => console.log("Error in DB", err));
 
 const PORT = 3000;
 app.listen(PORT, () => {
