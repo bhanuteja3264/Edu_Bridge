ce9ebce7cfeed78a6b7ca3c1ce0c2245072809f9
48-JASHWANTH <ratnajashwanth64@gmail.com>
2025-02-07 13:02:00 +0530

    teamsBackendEnd Updated

--- server/controllers/facultyController.js
+++ server/controllers/facultyController.js
@@ -1,11 +1,28 @@
+// FacultyController.js
 import mongoose from "mongoose";
 import asynchandler from "express-async-handler";
 import Faculty from "../models/FacultyModel.js";
 import SectionTeams from "../models/SectionTeamsModel.js";
+import Team from "../models/teamsModel.js";
 
-//New entitity created by faculty
+// New entity created by faculty
 export const createTeams = asynchandler(async (req, res) => {
-    const { classID, facultyID, year, sem, branch, section, projectType, noOfTeams, teams, projectTitles, noOfStudents } = req.body;
+    const { 
+        classID, 
+        facultyID, 
+        year, 
+        sem, 
+        branch, 
+        section, 
+        projectType, 
+        noOfTeams, 
+        teams, 
+        projectTitles, 
+        noOfStudents, 
+        guides, 
+        subject, 
+        guideApproval 
+    } = req.body;
 
     try {
         const updatedFaculty = await Faculty.findOneAndUpdate(
@@ -27,7 +44,7 @@ export const createTeams = asynchandler(async (req, res) => {
             projectType,
             facultyID,
             numberOfTeams: noOfTeams,
-            teamsList: teams, 
+            teamsList: teams,
             projectTitles: new Map(Object.entries(projectTitles)),
             numberOfStudents: noOfStudents,
             status: 'Pending'
@@ -35,10 +52,28 @@ export const createTeams = asynchandler(async (req, res) => {
 
         await newSectionTeam.save();
 
+        const teamPromises = Object.entries(teams).map(async ([teamId, studentList]) => {
+            const newTeam = new Team({
+                teamId,
+                listOfStudents: studentList,
+                projectTitle: projectTitles[teamId],
+                projectType,
+                subject,
+                githubURL: "", // Placeholder, assuming it will be updated later
+                guideApproval: guideApproval || false,
+                guideFacultyId: guides[teamId] || "",
+                inchargefacultyId: facultyID
+            });
+            return await newTeam.save();
+        });
+
+        const createdTeams = await Promise.all(teamPromises);
+
         return res.status(200).json({
-            message: 'ClassID added to leadedProjects and SectionTeams created successfully',
+            message: 'ClassID added to leadedProjects, SectionTeams and individual Teams created successfully',
             updatedFaculty,
-            newSectionTeam
+            newSectionTeam,
+            createdTeams
         });
 
     } catch (error) {
